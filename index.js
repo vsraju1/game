6b0c3f508334773766140e90579c079d0b330da6
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-08-17 22:04:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var button = document.querySelector("button");
+var print = document.querySelector("button").addEventListener("click", function(){
+    button.classList.add("click")
+    button.innerHTML = "Game undhaa Bhai??"
+}) 
